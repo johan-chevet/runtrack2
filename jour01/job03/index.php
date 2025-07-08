52a27f8e197843bbe31313fa243deb8d10a5b05a
Johan <johan.chevet@laplateforme.io>
2025-07-08 14:05:03 +0200

    remake

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -1,20 +1,48 @@
 <?php
-
-function getTableRow($name, $value): string {
-    $type = gettype($value);
-    return "<tr><td>$type</td><td>$name</td><td>$value</td></tr>";
-}
-
 $str = "Contenu de la variable";
 $int = 42;
 $pi = 3.14;
 $bool = true;
-$table = "<table><tr><th>type</th><th>nom</th><th>valeur</th></tr>";
-$table .= getTableRow("str", $str);
-$table .= getTableRow( "int", $int);
-$table .= getTableRow( "float", $float);
-$table .= getTableRow( "bool", $bool);
-$table .= "</table>";
 
-echo $table;
+$varDic = [
+    "str" => $str,
+    "int" => $int,
+    "pi" => $pi,
+    "bool" => $bool,
+]
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th> Type des variables</th>
+                    <th> Nom des variables </th>
+                    <th> Valeurs </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($varDic as $key => $value): ?>
+                    <tr>
+                        <td><?php echo gettype($value);?></td>
+                        <td><?php echo $key; ?></td>
+                        <td>
+                            <?php if (is_bool($value)) {
+                                echo $value ?"true":"false";
+                            } else {
+                                echo htmlspecialchars((string) $value);
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </body>
+</html>
