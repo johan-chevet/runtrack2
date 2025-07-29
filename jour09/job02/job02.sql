da29dd76d5ba3ba4c40c346e36a6b7b0bd78d6e1
johan <johan.chevet@laplateforme.io>
2025-07-29 16:49:11 +0200

    started day10

--- jour09/job02/job02.sql
+++ jour09/job02/job02.sql
@@ -66,13 +66,13 @@ CREATE TABLE IF NOT EXISTS `etudiants` (
 --
 
 INSERT INTO `etudiants` (`id`, `prenom`, `nom`, `naissance`, `sexe`, `email`) VALUES
-(2, 'Cyril', 'Zimmermann', '1989-01-02', 'Homme', 'cyril@laplateforme.io'),
-(3, 'Jessica', 'Soriano', '1995-09-08', 'Femme', 'jessica@laplateforme.io'),
-(4, 'Roxan', 'Roumégas', '2016-09-08', 'Homme', 'roxan@laplateforme.io'),
-(5, 'Pascal', 'Assens', '1999-12-31', 'Homme', 'pascal@laplateforme.io'),
-(6, 'Terry', 'Cristinelli', '2005-02-01', 'Homme', 'terry@laplateforme.io'),
-(7, 'Ruben', 'Habib', '1993-05-26', 'Homme', 'ruben.habib@laplateforme.io'),
-(8, 'Toto', 'Dupont', '2019-11-07', 'Homme', 'toto@laplateforme.io');
+(1, 'Cyril', 'Zimmermann', '1989-01-02', 'Homme', 'cyril@laplateforme.io'),
+(2, 'Jessica', 'Soriano', '1995-09-08', 'Femme', 'jessica@laplateforme.io'),
+(3, 'Roxan', 'Roumégas', '2016-09-08', 'Homme', 'roxan@laplateforme.io'),
+(4, 'Pascal', 'Assens', '1999-12-31', 'Homme', 'pascal@laplateforme.io'),
+(5, 'Terry', 'Cristinelli', '2005-02-01', 'Homme', 'terry@laplateforme.io'),
+(6, 'Ruben', 'Habib', '1993-05-26', 'Homme', 'ruben.habib@laplateforme.io'),
+(7, 'Toto', 'Dupont', '2019-11-07', 'Homme', 'toto@laplateforme.io');
 
 -- --------------------------------------------------------
 
---
+++ jour10/job05/index.php
@@ -0,0 +1,52 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "jour09");
+if ($mysqli->connect_errno) {
+    printf("Db connection error: ", $mysqli->connect_error);
+    exit(1);
+}
+try {
+    $query = $mysqli->query("select * from etudiants where timestampdiff(year,  naissance, now()) < 18");
+} catch (mysqli_sql_exception $e) {
+    echo "query error: " . $e->getMessage();
+    die();
+}
+$fields = $query->fetch_fields();
+$rows = $query->fetch_all();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>job05</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <?php foreach ($fields as $field): ?>
+                    <th>
+                        <?= $field->name ?>
+                    </th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($rows as $row): ?>
+                <tr>
+                    <?php foreach ($row as $data): ?>
+                        <td>
+                            <?= $data ?>
+                        </td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
