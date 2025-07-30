a1326de46c5e0401a98cac85757ba31f3c512d47
johan <johan.chevet@laplateforme.io>
2025-07-30 09:58:40 +0200

    fix: use cookie instead of session

--- jour08/job04/index.php
+++ jour08/job04/index.php
@@ -1,31 +1,34 @@
 <?php
 session_start();
 if (isset($_POST["prenom"]) && strlen($_POST["prenom"])) {
-    $_SESSION["login"] = $_POST["prenom"];
+    $_COOKIE["login"] = $_POST["prenom"];
 }
 if (isset($_POST["deco"])) {
-    $_SESSION["login"] = null;
+    $_COOKIE["login"] = null;
 }
 ?>
 
 <html lang="eng">
-    <head>
-        <meta charset="UTF-8">
-        <title>Job04</title>
-    </head>
-    <body>
-        <?php if (!isset($_SESSION["login"])): ?>
-            <form method="post">
-                <label for="prenom">Prenom:</label>
-                <input type="text" name="prenom" id="prenom">
-                <button type="submit" name="connexion" >Connexion</button>
-            </form>
-        <?php endif; ?>
-        <?php if (isset($_SESSION["login"])): ?>
-            <form method="post">
-                <p><?="Bonjour " . htmlspecialchars($_SESSION["login"]) . " !" ?></p>
-                <button type="submit" name="deco" >Déconnexion</button>
-            </form>
-        <?php endif; ?>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Job04</title>
+</head>
+
+<body>
+    <?php if (!isset($_COOKIE["login"])): ?>
+        <form method="post">
+            <label for="prenom">Prenom:</label>
+            <input type="text" name="prenom" id="prenom">
+            <button type="submit" name="connexion">Connexion</button>
+        </form>
+    <?php endif; ?>
+    <?php if (isset($_COOKIE["login"])): ?>
+        <form method="post">
+            <p><?= "Bonjour " . htmlspecialchars($_COOKIE["login"]) . " !" ?></p>
+            <button type="submit" name="deco">Déconnexion</button>
+        </form>
+    <?php endif; ?>
+</body>
+
 </html>
